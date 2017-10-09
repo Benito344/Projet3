7c5f1b7a7aa40a39e5d3eb3bcd09b8f88387ae09
Benito <behague.benjamin@gmail.com>
2017-10-09 11:14:06 +0200

    Add two methods : SaveData and LoadMood

--- app/src/main/java/com/behague/benjamin/projet3/model/DataManager.java
+++ app/src/main/java/com/behague/benjamin/projet3/model/DataManager.java
@@ -1,8 +1,25 @@
 package com.behague.benjamin.projet3.model;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 /**
  * Created by Benjamin BEHAGUE on 09/10/2017.
  */
 
 public class DataManager {
+    private static SharedPreferences mSaveMood;
+
+    public static void SaveData(Context context, String mWeekDay , int mNumColor, String mComment){
+        mSaveMood = context.getSharedPreferences("Moods",context.MODE_PRIVATE);
+        mSaveMood.edit().putInt(mWeekDay, mNumColor).apply();
+        mSaveMood.edit().putString(mWeekDay+1, mComment).apply();
+    }
+
+    public static int LoadMood(Context context, String mWeekDay){
+        int mNumColor;
+        mSaveMood = context.getSharedPreferences("Moods",context.MODE_PRIVATE);
+        mNumColor = mSaveMood.getInt(mWeekDay, 3);
+        return mNumColor ;
+    }
 }
