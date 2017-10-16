9aab09d43fef933220be7964d9da3940a5fb3e4b
Benito <behague.benjamin@gmail.com>
2017-10-16 10:18:17 +0200

    Add Commentary for explain the code of Pie Chart

--- app/src/main/java/com/behague/benjamin/projet3/Pie_Chart.java
+++ app/src/main/java/com/behague/benjamin/projet3/Pie_Chart.java
@@ -25,19 +25,28 @@ public class Pie_Chart extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pie_chart);
 
+        /*** Track the Pie Chart with her id. ***/
         PieChart mPieChart = (PieChart) findViewById(R.id.activity_pie_chart);
 
+        /*** Use DataManager for load data. ***/
         DataManager.loadMoods(this);
+
+        /*** The data are stocked into this Map with specific order (See getStatsMoods() in MoodList
+         *   for more explain. ***/
         Map<Integer, Integer> mStatsMoods =  MoodList.getStatsMoods();
 
+        /*** Init two ArrayList, one for stock the value ( in PieEntry ), the other for stock the colors
+         *   ( in Integer ). ***/
         ArrayList<PieEntry> entries = new ArrayList<>();
 
         ArrayList<Integer> colors = new ArrayList<>();
 
-        mPieChart.setRotationEnabled(true);
-
+        /*** Init mListLength for get the total number of Moods. It used for percentage numbers ***/
         int mListLength = MoodList.getLengthMoodList();
 
+        /*** Here we add data into the ArrayList "entries". For this we used a switch for detected what
+         *   type of moods is present ( e.getKey() ) and make the good color for each moods.
+         *   The calcul , e.getValue() * 100 / mListLength, it for convert to percentage ***/
         for(Map.Entry<Integer, Integer> e : mStatsMoods.entrySet()){
             switch (e.getKey()){
                 case 1 :
@@ -63,22 +72,30 @@ public class Pie_Chart extends AppCompatActivity {
             }
         }
 
-        PieDataSet dataset = new PieDataSet(entries, "Moods");
+        /*** Init a PieDataSet for give the value to the PieChart ***/
+        PieDataSet mDataSet = new PieDataSet(entries, "Moods");
 
-        dataset.setColors(colors);
-        dataset.setValueFormatter(new PercentFormatter());
-        dataset.setValueTextSize(15f);
-        dataset.setSliceSpace(2);
-        dataset.setValueTextColor(Color.BLACK);
+        /*** With mDataSet we can set other parameters like the colors, the style of value, the color
+         *   of value etc....***/
+        mDataSet.setColors(colors);
+        mDataSet.setValueFormatter(new PercentFormatter());
+        mDataSet.setValueTextSize(15f);
+        mDataSet.setSliceSpace(2);
+        mDataSet.setValueTextColor(Color.BLACK);
 
+        /*** Init the Legend of the Pie Chart. It's just custom the Legend (not necessary) ***/
         Legend mLegend = mPieChart.getLegend();
         mLegend.setTextSize(15f);
 
+        /*** Here we custom the Pie Chart with animation, rotation, we can add description or not
+         *   and other ... ***/
         mPieChart.animateY(2000,Easing.EasingOption.EaseOutBounce);
         mPieChart.setDrawHoleEnabled(false);
         mPieChart.getDescription().setEnabled(false);
+        mPieChart.setRotationEnabled(true);
 
-        PieData data = new PieData(dataset);
+        /*** Here we give all the parameters mDataSet (see before) to the PieChart ***/
+        PieData data = new PieData(mDataSet);
         mPieChart.setData(data);
         mPieChart.invalidate();
 
