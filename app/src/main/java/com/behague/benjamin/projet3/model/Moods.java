5c1af506afc3f62270cebdf3b5b1e9c03710fb18
Benito <behague.benjamin@gmail.com>
2017-10-18 17:30:51 +0200

    Deleted getId function

--- app/src/main/java/com/behague/benjamin/projet3/model/Moods.java
+++ app/src/main/java/com/behague/benjamin/projet3/model/Moods.java
@@ -7,28 +7,24 @@ import java.io.Serializable;
  */
 
 /*** These class is for create Moods Object ***/
-public class Moods implements Serializable{
+public class Moods implements Serializable {
     private int mMood, mId;
     private String mCommentary;
 
     /*** These is for parameter the Mood Object with his mood and commentary ***/
-    public Moods(int id ,int mood, String commentary){
+    public Moods(int id, int mood, String commentary) {
         this.mId = id;
         this.mMood = mood;
         this.mCommentary = commentary;
     }
 
     /*** These is a getter for retrieve the number of mood ***/
-    public int getMood(){
+    public int getMood() {
         return this.mMood;
     }
 
     /*** These is a getter for retrieve the commentary link at mood ***/
     public String getCommentary() {
         return this.mCommentary;
-    }
-
-    public int getId() {
-        return this.mId;
     }
 }
