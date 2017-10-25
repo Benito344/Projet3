11d566638dc56d2592652031adaf390b2192660f
Benito <behague.benjamin@gmail.com>
2017-10-25 13:40:30 +0200

    Modification de getMoodsHistoricTest

--- app/src/test/java/com/behague/benjamin/projet3/model/MoodListTest.java
+++ app/src/test/java/com/behague/benjamin/projet3/model/MoodListTest.java
@@ -13,7 +13,9 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
+import static com.behague.benjamin.projet3.model.MoodList.mMoodListData;
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
@@ -34,11 +36,13 @@ public class MoodListTest {
     @Before
     public void setUp() throws Exception {
         mockedListMoods = new MoodList();
-
+        mMoodListData.clear();
         mood1 = new Moods(185, 4, "test");
         mood2 = new Moods(186, 2, null);
         mockedListMoods.addMood(mood1);
         mockedListMoods.addMood(mood2);
+
+
         //doReturn(anyInt()).when(MoodList.getLengthMoodList());
         //when(mockedListMoods.getLengthMoodList()).thenReturn(anyInt());
         //when(mockedListMoods.addMood(mood1)).thenReturn(mood1.getMood());
@@ -47,22 +51,39 @@ public class MoodListTest {
 
     @Test
     public void addMoodTest() throws Exception {
-        assertNotNull(mockedListMoods.mMoodListData);
-        assertArrayEquals(mockedListMoods.mMoodListData.toArray(), new Moods []{mood1, mood2});
-    }
-
- /*   @Test
-    public void getMoodsHistoricTest() throws Exception {
-        List<Integer> tNumMoods = new ArrayList<>();
-        for (Moods t : MoodsListTest){
-            tNumMoods.add(t.getMood());
-        }
-        int [] excpectedValue = {4,2};
-
-        assertEquals(excpectedValue.length, tNumMoods.size());
+        assertNotNull(mMoodListData);
+        assertArrayEquals(mMoodListData.toArray(), new Moods []{mood1, mood2});
     }
 
     @Test
+    public void getMoodsHistoricTest() throws Exception {
+
+        int i = 0, sizeOfMoodList = mMoodListData.size();
+        if(sizeOfMoodList > 7){
+            int tMoods[] = new int [7];
+            List<Moods> tail = mMoodListData.subList(Math.max(mMoodListData.size() - 7, 0), mMoodListData.size());
+            for (Moods t : tail) {
+                if (t.getMood() != 0) {
+                    tMoods[i] = t.getMood();
+                }
+                i++;
+            }
+            assertNotNull(tMoods);
+        }
+        else{
+            int tMoods[] = new int[sizeOfMoodList];
+            for (Moods t : mMoodListData ){
+                if (t.getMood() != 0) {
+                    tMoods[i] = t.getMood();
+                }
+                i++;
+            }
+            assertNotNull(tMoods);
+            assertArrayEquals(tMoods, new int[] {4,2});
+        }
+    }
+
+ /*   @Test
     public void getMoodsCommsTest() throws Exception {
         List<String> tComsMoods = new ArrayList<>();
         for (Moods t : MoodsListTest){
@@ -97,7 +118,7 @@ public class MoodListTest {
 
     @Test
     public void getLengthMoodListTest() throws Exception {
-       // assertNotNull(mockedListMoods.getLengthMoodList());
+       assertNotNull(mockedListMoods.getLengthMoodList());
     }
 
 }
